30f211845ad8ebc8710fff6f2d46ef76f5394107
vladosfedulov <vladislav_fedulo@mail.ru>
2019-04-22 22:13:22 +0300

    v2

--- hello_my_friend.py
+++ hello_my_friend.py
@@ -1,68 +1,89 @@
-from flask import Flask, redirect, render_template, request
+from urllib.parse import urlencode
 import requests
+import os
+
+from flask import Flask, redirect, render_template, request
 
 
-SECRET = 'AzIgdi8l7n7PlpThoeVo'
+SECRET = os.environ['SECRET']
+
 ACCESS_TOKEN = ''
 USER_ID = ''
+CLIENT_ID = os.environ['CLIENT_ID']
+URL_GET_CODE = f"https://oauth.vk.com/authorize?{{}}"
+URL_GET_ACCESS_TOKEN = f"https://oauth.vk.com/access_token?{{}}"
 METHOD = "https://api.vk.com/method/{}&access_token={}&v=5.95"
+METHOD_USERS_GET = f"https://api.vk.com/method/users.get?{{}}"
+METHOD_FRIENDS_GET = f"https://api.vk.com/method/friends.get?{{}}"
+REDIRECT_URI = r'http://127.0.0.1:5000/vk_auth'
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    global USER_ID
-    global ACCESS_TOKEN
     if USER_ID:
-        method = "users.get?user_ids={}".format(USER_ID)
-        req = METHOD.format(method, ACCESS_TOKEN)
-        request_vk = requests.get(req)
-        dic = request_vk.json()
-        dic = dic['response']
-        return render_template('main_1.html', user=dic[0])
+        params = {
+            'user_ids': USER_ID,
+            'access_token': ACCESS_TOKEN,
+            'v': '5.95',
+        }
+        url = METHOD_USERS_GET.format(urlencode(params))
+        request_vk = requests.get(url)
+        result = request_vk.json()
+        result = result['response']
+        return render_template('main_1.html', user=result[0])
     else:
-        return render_template('main_0.html', user_id=USER_ID)
-
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % subpath
+        return render_template('main_0.html')
 
 
 @app.route('/user/<int:user_id>')
 def vk_access(user_id):
-    global ACCESS_TOKEN
-    global METHOD
-    method = "friends.get?user_id={}&count=5&order=random&fields=nickname".format(user_id)
-    req = METHOD.format(method, ACCESS_TOKEN)
-    request_vk = requests.get(req)
-    dic = request_vk.json()
-    dic = dic['response']['items']
+    params = {
+        'user_id': user_id,
+        'count': '5',
+        'order': 'random',
+        'fields': 'nickname',
+        'access_token': ACCESS_TOKEN,
+        'v': '5.95',
+    }
+    url = METHOD_FRIENDS_GET.format(urlencode(params))
 
-    method = "users.get?user_ids={}".format(USER_ID)
-    req = METHOD.format(method, ACCESS_TOKEN)
-    request_vk = requests.get(req)
-    dic_user = request_vk.json()
-    dic_user = dic_user['response']
-    return render_template("page.html", user=dic, login_user=dic_user[0])
+    request_vk = requests.get(url)
+    result = request_vk.json()
+    result = result['response']['items']
+
+    params_user = {
+        'user_ids': USER_ID,
+        'access_token': ACCESS_TOKEN,
+        'v': '5.95',
+    }
+    url_user = METHOD_USERS_GET.format(urlencode(params_user))
+    request_vk_user = requests.get(url_user)
+    result_user = request_vk_user.json()
+    result_user = result_user['response']
+    return render_template("page.html", user=result, login_user=result_user[0])
 
 
 @app.route('/vk_auth', methods=['POST', 'GET'])
 def vk_auth():
     global USER_ID
     global ACCESS_TOKEN
-
     code = request.args.get('code')
-    l = "https://oauth.vk.com/access_token?client_id=6952806&client_secret={}&redirect_uri=http://127.0.0.1:5000/vk_auth&code={}".format(SECRET, code)
-    req = requests.get(l)
-    dic = req.json()
-    ACCESS_TOKEN = dic['access_token']
-    USER_ID = dic['user_id']
-    l2 = "https://api.vk.com/method/friends.get?user_id={}&count=5&order=random&access_token={}&v=5.95".format(USER_ID, ACCESS_TOKEN)
-    req = requests.get(l2)
-    dic = req.json()
+    params = {
+        'client_id': CLIENT_ID,
+        'client_secret': SECRET,
+        'redirect_uri': REDIRECT_URI,
+        'code': code,
+
+    }
+
+    url = URL_GET_ACCESS_TOKEN.format(urlencode(params))
+    request_vk = requests.get(url)
+    result = request_vk.json()
+    ACCESS_TOKEN = result['access_token']
+    USER_ID = result['user_id']
 
     return redirect('/')
 
@@ -71,6 +92,14 @@ def vk_auth():
 def login():
     error = None
     if request.method == 'GET':
-        return redirect("https://oauth.vk.com/authorize?client_id=6952806&display=page&redirect_uri=http://127.0.0.1:5000/vk_auth&response_type=code&v=5.95")
+        params = {
+            'client_id': CLIENT_ID,
+            'display': 'page',
+            'redirect_uri': REDIRECT_URI,
+            'response_type': 'code',
+            'v': '5.95',
+        }
+        url = URL_GET_CODE.format(urlencode(params))
+        return redirect(url)
 
     return render_template('login.html', error=error)
